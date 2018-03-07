882b28763886853a1dc7e5a87ea1960cf5256c9b
Litvinenko Darya <Litvinenko10.10@mail.ru>
2018-03-07 19:02:28 +0300

    Пример из видео. Ошибка

--- web_app.py
+++ web_app.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import Flask
 
 from req import get_weather
@@ -12,9 +14,13 @@ app = Flask(__name__)
 def index():
 	url = 'http://api.openweathermap.org/data/2.5/weather?id=%s&APPID=%s&units=metric'%(city_id, apikey)
 	weather = get_weather(url)
+
 	print(weather)
-	resul = 'Температура: %s\n' %weather['main']['temp']
-	resul = 'Город: %s' %weather['name']
+	
+	cur_date = datetime.now().strftime('%d.%m.%Y')
+	result = '<p><b>Температура:</b> %s</p>' %weather['main']['temp']
+	result += '<p><b>Город:</b> %s</p>' %weather['name']
+	result += '<p><b>Дата:</b> %s</p>' %cur_date
 	return result
 
 if __name__ == "__main__":
